7fa3fc4ee622c116c0ba45f7a804c16850d1b96b
Grigoriev Egor <grigoriev.egor2596@gmail.com>
2022-11-23 21:29:56 +0300

    added dataSourse filed and closing resources

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -15,10 +15,10 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SimpleJDBCRepository {
-
-    private Connection connection = null;
-    private PreparedStatement ps = null;
-    private Statement st = null;
+    private CustomDataSource dataSource;
+    private Connection connection;
+    private PreparedStatement ps;
+    private Statement st;
 
     private static final String createUserSQL = "INSERT INTO users (firstname, lastname, age) VALUES(?, ?, ?)";
     private static final String updateUserSQL = "UPDATE users SET firstname = ?, lastname = ?, age = ? WHERE id = ?";
@@ -30,6 +30,7 @@ public class SimpleJDBCRepository {
     public Long createUser(User user) {
         Long result = null;
         try {
+            connection = dataSource.getConnection();
             ps = connection.prepareStatement(createUserSQL);
             ps.setString(1, user.getFirstName());
             ps.setString(2, user.getLastName());
@@ -37,6 +38,9 @@ public class SimpleJDBCRepository {
             result = (long) ps.executeUpdate();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } finally {
+            try { ps.close(); } catch (Exception e) { /* Ignored */ }
+            try { connection.close(); } catch (Exception e) { /* Ignored */ }
         }
         return result;
     }
@@ -45,6 +49,7 @@ public class SimpleJDBCRepository {
         ResultSet rs = null;
         User user = null;
         try {
+            connection = dataSource.getConnection();
             ps = connection.prepareStatement(findUserByIdSQL);
             ps.setString(1, String.valueOf(userId));
             rs = ps.executeQuery();
@@ -57,6 +62,10 @@ public class SimpleJDBCRepository {
             user = new User(userId, firstname, lastname, age);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } finally {
+            try { rs.close(); } catch (Exception e) {}
+            try { ps.close(); } catch (Exception e) {}
+            try { connection.close(); } catch (Exception e) {}
         }
         return user;
     }
@@ -65,6 +74,7 @@ public class SimpleJDBCRepository {
         ResultSet rs = null;
         User user = null;
         try {
+            connection = dataSource.getConnection();
             ps = connection.prepareStatement(findUserByNameSQL);
             ps.setString(1, userName);
             rs = ps.executeQuery();
@@ -78,15 +88,21 @@ public class SimpleJDBCRepository {
             user = new User(id, firstname, lastname, age);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } finally {
+            try { rs.close(); } catch (Exception e) {}
+            try { ps.close(); } catch (Exception e) {}
+            try { connection.close(); } catch (Exception e) {}
         }
         return user;
     }
 
     public List<User> findAllUser() {
         List<User> users = null;
+        ResultSet rs = null;
         try {
+            connection = dataSource.getConnection();
             st = connection.createStatement();
-            ResultSet rs = st.executeQuery(findAllUserSQL);
+            rs = st.executeQuery(findAllUserSQL);
             if (!rs.next()) throw new SQLException("There are no users!");
             users = new ArrayList<User>();
             while (rs.next()) {
@@ -98,12 +114,17 @@ public class SimpleJDBCRepository {
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } finally {
+            try { rs.close(); } catch (Exception e) {}
+            try { ps.close(); } catch (Exception e) {}
+            try { connection.close(); } catch (Exception e) {}
         }
         return users;
     }
 
     public User updateUser(User user) {
         try {
+            connection = dataSource.getConnection();
             ps = connection.prepareStatement(updateUserSQL);
             ps.setString(1, user.getFirstName());
             ps.setString(2, user.getLastName());
@@ -112,17 +133,24 @@ public class SimpleJDBCRepository {
             if (ps.executeUpdate() == 0) throw new SQLException("No such user exists");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } finally {
+            try { ps.close(); } catch (Exception e) {}
+            try { connection.close(); } catch (Exception e) {}
         }
         return user;
     }
 
     public void deleteUser(Long userId) {
         try {
+            connection = dataSource.getConnection();
             ps = connection.prepareStatement(deleteUser);
             ps.setString(1, String.valueOf(userId));
             if (ps.executeUpdate() == 0) throw new SQLException("No such user exists");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } finally {
+            try { ps.close(); } catch (Exception e) {}
+            try { connection.close(); } catch (Exception e) {}
         }
     }
 
